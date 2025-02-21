ef59f1cd2388caf80c97ded1dfc233bab89e94ca
gkgautam11 <test@gmail.com>
2025-02-21 19:54:18 +0530

    Parameter added

--- src/main/java/jpetStore/UIDemo.java
+++ src/main/java/jpetStore/UIDemo.java
@@ -2,18 +2,35 @@ package jpetStore;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UIDemo {
 	
+	@Parameters("Browser")
 	@Test
-	public void verifyPageTitle()
+	public void verifyPageTitle(String BrowserName)
 	{
+		
 		WebDriverManager.chromiumdriver().setup();
-//		WebDriver driver = new ChromeDriver();
-		WebDriver driver= new ChromeDriver();
+		WebDriver driver= null;
+		
+		if(BrowserName.contains("Edge"))
+		{
+			WebDriverManager.chromiumdriver().setup();
+			 driver= new EdgeDriver();
+			
+		}
+		
+		else if(BrowserName.contains("Chrome"))
+		{
+			 driver= new ChromeDriver();
+		}
+		
+		
 		
 		driver.get("https://jpetstore.aspectran.com/");
 		
